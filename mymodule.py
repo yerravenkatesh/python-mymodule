9a62c6a9d3236751c247c4cef2d40f5b99f843dc
yerravenkatesh <103556534+yerravenkatesh@users.noreply.github.com>
2022-04-13 17:40:45 +0800

    Added another function of postal lookup
    Postal lookup takes in postal code and return latitude and longitude

--- mymodule.py
+++ mymodule.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen import json
 api_key = "xxxxxxx"
+
 def get_weather(city):
   sock = urlopen("http://api.openweathermap.org/data/2.5/weather?q=" +
                  city + "&appid=" + api_key)
@@ -7,7 +8,16 @@ def get_weather(city):
   sock.close()
   weather = json.loads(result)
   return weather["main"]["temp"] -273.15
+def postal_lookup(postal_code):
+  sock = urlopen("http://api.postcodes.io/postcodes/" + postal_code) 
+  result = sock.read()
+  sock.close()
+  details = json.loads(result)
+  return (details["result"]["latitude"], details["result"]["longitude"])
 
 if __name__ == "__main__": 
   degrees = get_weather("OSLO")
   print("Weather in Oslo is %.2f degrees Celsius" % degrees)
+  location = postal_lookup("B323PP")
+  print(location)
+ 
